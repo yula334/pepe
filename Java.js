78cfd777330a6a137f427bef62136180f5c3cb18
olaa321 <olaa15444@gmail.com>
2024-04-21 18:01:00 +0300

    d

--- Java.js
+++ Java.js
@@ -16,7 +16,13 @@ class Pll{
     }
 }
 
-let curquest = [new Pll("Black", "Блок", "Назад", "Чёрный"), new Pll("Table", "Таблетка", "Керамика", "Стол"), new Pll("Look", "Лук", "Закрывать", "Смотреть")]
+
+let curquests = [new Pll("Black", "Блок", "Назад", "Чёрный"), new Pll("Table", "Таблетка", "Керамика", "Стол"), new Pll("Look", "Лук", "Закрывать", "Смотреть")]
+
+let tag = 0
+
+let curquest = curquests[tag]
+curquest.display()
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -28,3 +34,28 @@ function shuffle(array) {
     }
     return array;
 }
+for (let i = 0; i < anss.length; i += 1){ 
+    anss[i].addEventListener("click", function(){ 
+        if (anss[i].innerHTML == curquest.correct){ 
+            anss[i].style.background = "#32CD32" 
+            anime({
+                targets: anss[i],
+                background: "#4A4957",
+                duration: 500,
+                easing: "linear",
+            })
+            
+        } else { 
+            anss[i].style.background = "#DC143C"  
+            anime({
+                targets: anss[i],
+                background: "#4A4957",
+                duration: 500,
+                easing: "linear",
+            })
+        } 
+        tag += 1
+        curquest = curquests[tag]
+        curquest.display() 
+    }) 
+} 
